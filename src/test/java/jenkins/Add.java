d4c7f7f94f0e8b4ede1a59e5718ff6f69763b0f6
siva8122008@gmail <siva8122008@gmail.com>
2025-08-12 12:08:48 +0530

    New Commit4

---
+++ src/test/java/jenkins/Add.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+	}
+
+}
