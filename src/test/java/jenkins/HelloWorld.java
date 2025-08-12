1a27d2ab1589bf1dec4adcfcc1ca58e693512379
siva8122008@gmail <siva8122008@gmail.com>
2025-08-12 11:04:30 +0530

    New Commit3

--- src/test/java/jenkins/HelloWorld.java
+++ src/test/java/jenkins/HelloWorld.java
@@ -1,9 +1,12 @@
 package jenkins;
 
-public class HelloWorld {
+import org.testng.annotations.Test;
 
+public class HelloWorld {
+    @Test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
 
 	}
 
